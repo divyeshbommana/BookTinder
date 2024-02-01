89b6f51c5c5bd43000f857ba7647c82771cb9f02
divyeshbommana <divyeshbommana@gmail.com>
2024-02-01 15:34:40 -0500

    Gets random book and displays the name when "GET BOOK" is clicked

--- app/src/main/java/com/example/pracbook/Main.kt
+++ app/src/main/java/com/example/pracbook/Main.kt
@@ -3,14 +3,21 @@ package com.example.pracbook
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.database
+import kotlin.random.Random
 
 class Main : AppCompatActivity() {
     val auth = FirebaseAuth.getInstance()
+//    private lateinit var database: DatabaseReference
+    var database = Firebase.database.reference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +26,10 @@ class Main : AppCompatActivity() {
         val button = findViewById<Button>(R.id.logout)
         val textView = findViewById<TextView>(R.id.user_details)
         val user = auth.getCurrentUser();
+
+        val getBookButton = findViewById<Button>(R.id.btn_getbook)
+        val bookTextView = findViewById<TextView>(R.id.book)
+
         if(user == null){
             val intent = Intent(getApplicationContext(), Login::class.java)
             startActivity(intent)
@@ -32,6 +43,19 @@ class Main : AppCompatActivity() {
             val intent = Intent(getApplicationContext(), Login::class.java)
             startActivity(intent)
             finish()
+        }
+
+        getBookButton.setOnClickListener {
+            var randomIndex = Random.nextInt(0,10000)
+            database.child("Books").child("0").child(randomIndex.toString()).get().addOnSuccessListener {
+                bookTextView.setText(it.child("actualTitle").value.toString())
+            }.addOnFailureListener {
+                Toast.makeText(
+                    baseContext,
+                    "Error getting data",
+                    Toast.LENGTH_SHORT,
+                ).show()
+            }
         }
     }
 }
