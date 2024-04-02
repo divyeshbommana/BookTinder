1e62c2027f1d4a33753133882d2d246bf7b969a3
Jashnavi Bommana <91922122+bommana401@users.noreply.github.com>
2024-04-02 18:59:31 -0400

    Most recent UI has swipe and flip functionality.

--- app/src/main/java/com/example/pracbook/Home.kt
+++ app/src/main/java/com/example/pracbook/Home.kt
@@ -1,5 +1,8 @@
 package com.example.pracbook
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -10,6 +13,8 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.cardview.widget.CardView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
@@ -37,6 +42,11 @@ fun getRecIndex(): Int {
 }
 class Home : Fragment() {
 
+    //Code related to flip.
+    private lateinit var bookInfoCard: CardView
+    private var isBookCoverVisible = true
+    private lateinit var bookCoverImage: ImageView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -48,15 +58,20 @@ class Home : Fragment() {
         // Initilizes Firebase realtime database extension to database variable
         var database = Firebase.database.reference
 
-
         val button = view.findViewById<Button>(R.id.logout)
 //        val textViewUserDetails = view.findViewById<TextView>(R.id.user_details)
         val getBookButton = view.findViewById<Button>(R.id.btn_getbook)
         val likeBookButton = view.findViewById<Button>(R.id.btn_likebook)
         val dislikeBookButton = view.findViewById<Button>(R.id.btn_dislikebook)
         val bookTextView = view.findViewById<TextView>(R.id.book)
-        val bookCoverImage = view.findViewById<ImageView>(R.id.book_cover)
+        bookCoverImage = view.findViewById(R.id.book_cover)
 
+        //book swipe and flip mechnaism:
+        val bookTitleTextView = view.findViewById<TextView>(R.id.book_title)
+        val bookDescription = view.findViewById<TextView>(R.id.book_description)
+
+        // flip card pointer.
+        bookInfoCard = view.findViewById(R.id.book_info_card)
 
         val user = auth.currentUser
         if (user == null) {
@@ -73,12 +88,243 @@ class Home : Fragment() {
             requireActivity().finish()
         }
 
+//        //we set teh first book as the starting book.
+//        val randomVal = Random.nextInt(0,10000)
+//        database.child("Books").child("0").child(randomVal.toString()).get().addOnSuccessListener {
+//            //sets the text on the card flipped tp the title of the book.
+//            bookTitleTextView.setText(it.child("actualTitle").value.toString())
+//            // sets the book description
+//            bookDescription.setText(it.child("actualGenre").value.toString())
+//            // Gets book URL and loads it into image view
+//            val url = it.child("img").value.toString()
+//            Picasso.with(requireActivity()).load(url).into(bookCoverImage)
+//        }.addOnFailureListener {
+//            Toast.makeText(
+//                requireContext(),
+//                "Error getting data",
+//                Toast.LENGTH_SHORT,
+//            ).show()
+//        }
+
+        // code for swipe mechanism. Class onSwipeTouchListener is used for this.
+        // Which every book is displyed, we see that and can user the features of swipeUp, swipeLeft, and swipeRight
+        bookCoverImage.setOnTouchListener(object : OnSwipeTouchListener(requireActivity()) { //this@Main to requireActivity()
+            override fun onSwipeLeft() {
+                super.onSwipeLeft()
+                //Dislike Book code
+                // Gets the data of the user in UserPreferences based on user's UID
+                // If new user, creates a new element in data base
+                val data = database.child("UserPreferences").child(auth.currentUser?.uid.toString()).child("Disliked")
+                // Pushes disliked book's title into "Disliked" subsection in database
+                data.push().setValue(bookTitleTextView.getText().toString()).addOnSuccessListener {//chnaged this from bookTextView to bookTitleTextView
+                    Toast.makeText(
+                        requireContext(),
+                        "Added book to disliked",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+
+                    //displays a new books after you swipe.
+                    val randomIndex = Random.nextInt(0,10000)
+                    // Gets data inside index
+                    database.child("Books").child("0").child(randomIndex.toString()).get().addOnSuccessListener {
+//                        // Sets text view to title of book
+//                        bookTextView.setText(it.child("actualTitle").value.toString())
+                        //sets the text on the card flipped tp the title of the book.
+                        bookTitleTextView.setText(it.child("actualTitle").value.toString())
+                        // sets the book description
+                        bookDescription.setText(it.child("actualGenre").value.toString())
+                        // Gets book URL and loads it into image view
+                        val url = it.child("img").value.toString()
+                        Picasso.with(requireActivity()).load(url).into(bookCoverImage)
+
+                    }
+                }.addOnFailureListener{
+                    Toast.makeText(
+                        requireContext(),
+                        "Unable to add book to disliked",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+
+            // Liked Book code
+            override fun onSwipeRight() {
+                super.onSwipeRight()
+                val data = database.child("UserPreferences").child(auth.currentUser?.uid.toString()).child("Liked")
+                // Pushes liked book's title into "Liked" subsection in database
+                data.push().setValue(bookTitleTextView.getText().toString()).addOnSuccessListener {
+                    Toast.makeText(
+                        requireContext(),
+                        "Added book to liked",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+
+                    //dispalys a new books
+                    val randomIndex = Random.nextInt(0,10000)
+                    // Gets data inside index
+                    database.child("Books").child("0").child(randomIndex.toString()).get().addOnSuccessListener {
+//                        // Sets text view to title of book
+//                        bookTextView.setText(it.child("actualTitle").value.toString())
+                        //sets the text on the card flipped tp the title of the book.
+                        bookTitleTextView.setText(it.child("actualTitle").value.toString())
+                        // sets the book description
+                        bookDescription.setText(it.child("actualGenre").value.toString())
+                        // Gets book URL and loads it into image view
+                        val url = it.child("img").value.toString()
+                        Picasso.with(requireActivity()).load(url).into(bookCoverImage)
+                    }
+                }.addOnFailureListener{
+                    Toast.makeText(
+                        requireContext(),
+                        "Unable to add book to liked",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+
+            // getBook method
+            override fun onSwipeUp() {
+                super.onSwipeUp()
+                // Gets a random integer from 0-9999
+                val randomIndex = Random.nextInt(0,10000)
+                // Gets data inside index
+                database.child("Books").child("0").child(randomIndex.toString()).get().addOnSuccessListener {
+//                    // Sets text view to title of book
+//                    bookTextView.setText(it.child("actualTitle").value.toString())
+                    //sets the text on the card flipped tp the title of the book.
+                    bookTitleTextView.setText(it.child("actualTitle").value.toString())
+                    // sets the book description
+                    bookDescription.setText(it.child("actualGenre").value.toString())
+                    // Gets book URL and loads it into image view
+                    val url = it.child("img").value.toString()
+                    Picasso.with(requireActivity()).load(url).into(bookCoverImage)
+                }.addOnFailureListener {
+                    Toast.makeText(
+                        requireContext(),
+                        "Error getting data",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+
+            override fun onClick() {
+                super.onClick()
+                // When user taps on the bookCoverImage, it should flip to display bookInformationCard. How do I this?
+                Toast.makeText(requireActivity(), "User Tapped on this on book image", Toast.LENGTH_SHORT)
+                    .show()
+                //method to flip card.
+                flipViews()
+            }
+        })
+
+        // code for book info card.
+        bookInfoCard.setOnTouchListener(object : OnSwipeTouchListener(requireActivity()) {
+            override fun onSwipeLeft() {
+                super.onSwipeLeft()
+                // Handle left swipe action on bookInformationCard
+                val data = database.child("UserPreferences").child(auth.currentUser?.uid.toString()).child("Disliked")
+                // Pushes disliked book's title into "Disliked" subsection in database
+                //bookTextView to bookTitleTextView
+                data.push().setValue(bookTitleTextView.getText().toString()).addOnSuccessListener {
+                    Toast.makeText(
+                        requireContext(),
+                        "Added book to disliked",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+
+                    //displays a new books after you swipe.
+                    val randomIndex = Random.nextInt(0,10000)
+                    // Gets data inside index
+                    database.child("Books").child("0").child(randomIndex.toString()).get().addOnSuccessListener {
+//                        // Sets text view to title of book
+//                        bookTextView.setText(it.child("actualTitle").value.toString())
+                        // Gets book URL and loads it into image view
+                        val url = it.child("img").value.toString()
+                        Picasso.with(requireActivity()).load(url).into(bookCoverImage)
+                    }
+                }.addOnFailureListener{
+                    Toast.makeText(
+                        requireContext(),
+                        "Unable to add book to disliked",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+
+                flipViews()
+            }
+
+            override fun onSwipeRight() {
+                super.onSwipeRight()
+                // Handle right swipe action on bookInformationCard (if needed)
+
+                val data = database.child("UserPreferences").child(auth.currentUser?.uid.toString()).child("Liked")
+                // Pushes liked book's title into "Liked" subsection in database
+                // bookTextView to bookTitleTextView
+                data.push().setValue(bookTitleTextView.getText().toString()).addOnSuccessListener {
+                    Toast.makeText(
+                        requireContext(),
+                        "Added book to liked",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+
+                    //dispalys a new books
+                    val randomIndex = Random.nextInt(0,10000)
+                    // Gets data inside index
+                    database.child("Books").child("0").child(randomIndex.toString()).get().addOnSuccessListener {
+//                        // Sets text view to title of book
+//                        bookTextView.setText(it.child("actualTitle").value.toString())
+                        // Gets book URL and loads it into image view
+                        val url = it.child("img").value.toString()
+                        Picasso.with(requireActivity()).load(url).into(bookCoverImage)
+                    }
+                }.addOnFailureListener{
+                    Toast.makeText(
+                        requireContext(),
+                        "Unable to add book to liked",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+
+                flipViews()
+            }
+
+            override fun onSwipeUp() {
+                super.onSwipeUp()
+                // Handle up swipe action on bookInformationCard (if needed)
+
+                val randomIndex = Random.nextInt(0,10000)
+                // Gets data inside index
+                database.child("Books").child("0").child(randomIndex.toString()).get().addOnSuccessListener {
+//                    // Sets text view to title of book
+//                    bookTextView.setText(it.child("actualTitle").value.toString())
+                    // Gets book URL and loads it into image view
+                    val url = it.child("img").value.toString()
+                    Picasso.with(requireActivity()).load(url).into(bookCoverImage)
+                }.addOnFailureListener {
+                    Toast.makeText(
+                        requireContext(),
+                        "Error getting data",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+                //flips once its clicked on.
+                flipViews()
+            }
+
+            override fun onClick() {
+                super.onClick()
+                // When user taps on the bookCoverImage, it should flip to display bookInformationCard. How do I this?
+//                Toast.makeText(requireActivity(), "User Tapped on this on the book info card", Toast.LENGTH_SHORT)
+//                    .show()
+                //method to flip card.I want this to display a new
+                flipViews()
+            }
+        })
+
 
 
         getBookButton.setOnClickListener {
-
             var viewedBooks = mutableListOf<String>()
-
             database.child("UserPreferences").child(auth.currentUser?.uid.toString()).child("Viewed").get().addOnSuccessListener {
                 for (x in it.children){
                     viewedBooks.add(x.value.toString())
@@ -186,9 +432,6 @@ class Home : Fragment() {
                 }
             }
         }
-
-
-
 
 
         likeBookButton.setOnClickListener {
@@ -363,6 +606,39 @@ class Home : Fragment() {
     override fun onDestroy() {
         super.onDestroy()
         Log.d("Lifecycle", "onDestroy() in MainActivity is called")
+    }
+
+    //New code to flip bookCoverImage to bookInformationCards
+    private fun flipViews() {
+        val fromView: View
+        val toView: View
+
+        if (isBookCoverVisible) {
+            fromView = bookCoverImage
+            toView = bookInfoCard
+        } else {
+            fromView = bookInfoCard
+            toView = bookCoverImage
+        }
+
+        isBookCoverVisible = !isBookCoverVisible
+
+        val flipOut = ObjectAnimator.ofFloat(fromView, "rotationY", 0f, 90f).apply {
+            duration = 500
+        }
+        val flipIn = ObjectAnimator.ofFloat(toView, "rotationY", -90f, 0f).apply {
+            duration = 500
+        }
+
+        flipOut.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                super.onAnimationEnd(animation)
+                fromView.isVisible = false
+                flipIn.start()
+                toView.isVisible = true
+            }
+        })
+        flipOut.start()
     }
 }
 
--- app/src/main/java/com/example/pracbook/Library.kt
+++ app/src/main/java/com/example/pracbook/Library.kt
@@ -1,10 +1,37 @@
 package com.example.pracbook
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.database
+
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
+//import android.graphics.BitmapFactory
+import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.cardview.widget.CardView
+//import com.google.firebase.auth.auth
+//import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.database
+import com.squareup.picasso.Picasso
+//import java.net.URL
+import kotlin.random.Random
+//import android.widget.RelativeLayout
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,18 +43,16 @@ private const val ARG_PARAM2 = "param2"
  * Use the [Library.newInstance] factory method to
  * create an instance of this fragment.
  */
+//changed this from Fragment()
 class Library : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    val auth = FirebaseAuth.getInstance()
+    // Initilizes Firebase realtime database extension to database variable
+    var database = Firebase.database.reference
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,23 +62,45 @@ class Library : Fragment() {
         return inflater.inflate(R.layout.fragment_library, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Library.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Library().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Find your views here
+        val tobereadBtn = view.findViewById<Button>(R.id.MyLibraryButton1)
+
+        tobereadBtn.setOnClickListener {
+            val intent = Intent(requireActivity(), ToBeRead::class.java)
+            startActivity(intent)
+        }
     }
+
+
+
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_library, container, false)
+//    }
+
+//    companion object {
+//        /**
+//         * Use this factory method to create a new instance of
+//         * this fragment using the provided parameters.
+//         *
+//         * @param param1 Parameter 1.
+//         * @param param2 Parameter 2.
+//         * @return A new instance of fragment Library.
+//         */
+//        // TODO: Rename and change types and number of parameters
+//        @JvmStatic
+//        fun newInstance(param1: String, param2: String) =
+//            Library().apply {
+//                arguments = Bundle().apply {
+//                    putString(ARG_PARAM1, param1)
+//                    putString(ARG_PARAM2, param2)
+//                }
+//            }
+//    }
 }
